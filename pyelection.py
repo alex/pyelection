617ea2da236607fa7348c9c707387e42c3c7e1bc
Alex Gaynor <alex@alex-desktop-linux.(none)>
2008-02-27 13:59:51 -0600

    Initial code ocommit for threading branch

--- pyelection.py
+++ pyelection.py
@@ -13,27 +13,29 @@ import threading
 from models import StateResults
 from states import STATES
 
-gobject.threads_init()
+gtk.gdk.threads_init()
 
+def threaded(func):
+    def decorator(*args, **kwargs):
+        t = threading.Thread(target=func, args=args, kwargs=kwargs)
+        t.start()
+    return decorator
 
 class pyelection(object):
     def __init__(self):
+        # Set up glade stuff
         self.gladefile = 'pyelection.glade'
         self.wTree = gtk.glade.XML(self.gladefile, 'mainWindow')
         
         self.wTree.signal_autoconnect(self)
         
+        # initiate widgets
         self.initiate_widgets()
-        
-        self.refreshing = False
-        
-        self.active_processes = []
     
     def initiate_widgets(self):
         self.stateView = self.wTree.get_widget('stateView')
         self.resultView = self.wTree.get_widget('resultView')
         self.overallView = self.wTree.get_widget('overallView')
-        
         
         self.stateList = gtk.ListStore(gobject.TYPE_PYOBJECT, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_INT, gobject.TYPE_STRING)
         self.state_columns = []
@@ -60,10 +62,11 @@ class pyelection(object):
             self.resultView.append_column(col)
         self.resultView.set_model(self.resultList)
         
-        self.overallList = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_INT)
+        self.overallList = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_INT, gobject.TYPE_INT)
         self.overall_columns = []
         self.overall_columns.append(('Name', 0))
-        self.overall_columns.append(('Delegates', 1))
+        self.overall_columns.append(('Votes', 1))
+        self.overall_columns.append(('Delegates', 2))
         for column in self.overall_columns:
             col = gtk.TreeViewColumn(column[0], gtk.CellRendererText(), text=column[1])
             col.set_resizable(True)
@@ -72,8 +75,6 @@ class pyelection(object):
         self.overallView.set_model(self.overallList)
         
         self.progress = self.wTree.get_widget('updateBar')
-        self.progress_percent = 0
-        self.progress_text = 'Not currently doing anything'
     
     def party_changed(self, widget):
         self.clear_states()
@@ -82,51 +83,67 @@ class pyelection(object):
             party = 'D'
         else:
             party = 'R'
-        self.stop_refresh()
-        self.idle_func(self.set_states(party))
+        self.total = len(STATES)
+        self.count = float(0)
+        
+        percent = self.count/self.total
+        text = 'Updated %d of %d states: %.0f%% done' % (self.count, self.total, percent*100)
+        self.refresh_progress_bar(percent, text)
+        for state in STATES:
+            self.add_state(state[0], state[1], party, count=True)
     
-    def set_states(self, party):
-        if not self.refreshing:
-            self.refreshing = True
-            num = len(STATES)
-            for count, state in enumerate(STATES):
-                state = StateResults(state[0], state[1], party)
-                self.stateList.append(state.get_list())
-                
-                count = float(count+1)
-                self.progress_text = 'Updated %d of %d states: %.0f%% done' % (count, num, count/num*100)
-                self.progress_percent = count / num
-                yield True
-            self.overall_update()
-            self.refreshing = False
-            yield False
-        
-    def state_changed(self, *args):
-        self.clear_results()
-        selection = self.stateView.get_selection()
-        
-        model, selection_iter = selection.get_selected()
-        
-        if selection_iter:
-            state = self.stateList.get_value(selection_iter, 0)
-            
-            for candidate in state.candidates:
-                self.resultList.append([candidate['name'], candidate['votes'], candidate['delegates']])
+    def state_changed(self, widget):
+        self.refresh_results()
     
     def refresh(self, widget):
-        self.idle_func(self.update_states())
+        self.total = len(list(self.iter_states()))
+        self.count = float(0)
+        percent = self.count/self.total
+        text = 'Updated %d of %d states: %.0f%% done' % (self.count, self.total, percent*100)
+        self.refresh_progress_bar(percent, text)
+        for itera, state in self.iter_states():
+            self.update_single(itera, count=True)
     
     def refresh_selected(self, widget):
-        selection = self.stateView.get_selection()
-        model, selection_iter = selection.get_selected()
+        model, itera = self.stateView.get_selection().get_selected()
         
-        self.refresh_single(selection_iter)
-        self.state_changed()
+        self.update_single(itera)
+        self.refresh_results()
     
-    def refresh_single(self, itera):
+    def custom_refresh(self, widget):
+        states = list(self.iter_states())
+        dlg = CustomRefreshDlg(states)
+        result = dlg.run()
+        [self.update_single(itera) for itera, state in result]
+        self.refresh_results()
+    
+    @threaded
+    def add_state(self, abbr, name, party, count=False):
+        state = StateResults(abbr, name, party)
+        
+        gtk.gdk.threads_enter()
+        self.stateList.append(state.get_list())
+        gtk.gdk.threads_leave()
+        
+        if count:
+            self.count += 1
+            percent = self.count/self.total
+            text = 'Updated %d of %d states: %.0f%% done' % (self.count, self.total, percent*100)
+            gtk.gdk.threads_enter()
+            self.refresh_progress_bar(percent, text)
+            gtk.gdk.threads_leave()
+        self.refresh_overall()
+    
+    @threaded
+    def update_single(self, itera, count=False):
+        gtk.gdk.threads_enter()
         state = self.stateList.get_value(itera, 0)
+        gtk.gdk.threads_leave()
+        
         state.refresh()
         info = state.get_list()
+        
+        gtk.gdk.threads_enter()
         self.stateList.set(itera,
             0, info[0],
             1, info[1],
@@ -134,70 +151,56 @@ class pyelection(object):
             3, info[3],
             4, info[4]
         )
+        gtk.gdk.threads_leave()
+        if count:
+            self.count += 1
+            percent = self.count/self.total
+            text = 'Updated %d of %d states: %.0f%% done' % (self.count, self.total, percent*100)
+            gtk.gdk.threads_enter()
+            self.refresh_progress_bar(percent, text)
+            gtk.gdk.threads_leave()
+        self.refresh_overall()
     
-    def update_states(self):
-        if not self.refreshing:
-            self.refreshing = True
-            num = len(app.stateList)
-            for count, (itera, state) in enumerate(self.iter_states()):
-                self.refresh_single(itera)
-                
-                count = float(count+1)
-                self.progress_text = 'Updated %d of %d states: %.0f%% done' % (count, num, count/num*100)
-                self.progress_percent = count / num
-                yield True
-            self.overall_update()
-            self.refreshing = False
-            yield False
+    def refresh_progress_bar(self, percent, text):
+        self.progress.set_fraction(percent)
+        if percent == 1 or percent == 1.0:
+            self.progress.set_text('Done')
+        else:
+            self.progress.set_text(text)
     
-    def overall_update(self):
-        self.clear_overall()
+    @threaded
+    def refresh_results(self):
+        gtk.gdk.threads_enter()
+        self.clear_results()
+        model, itera = self.stateView.get_selection().get_selected()
+        
+        if itera:
+            state = self.stateList.get_value(itera, 0)
+            for candidate in state.candidates:
+                self.resultList.append([candidate['name'], candidate['votes'], candidate['delegates']])
+        gtk.gdk.threads_leave()
+    
+    def refresh_overall(self):
         info = {}
         for itera, state in self.iter_states():
             for candidate in state.candidates:
                 if candidate['delegates'] > 0:
                     if info.has_key(candidate['name']):
                         info[candidate['name']]['delegates'] += candidate['delegates']
+                        info[candidate['name']]['votes'] += candidate['votes']
                     else:
                         info[candidate['name']] = {}
                         info[candidate['name']]['name'] = candidate['name']
                         info[candidate['name']]['delegates'] = candidate['delegates']
+                        info[candidate['name']]['votes'] = candidate['votes']
+        self.clear_overall()
         for cand in info.itervalues():
-            self.overallList.append([cand['name'], cand['delegates']])
-    
-    def custom_refresh(self, widget):
-        states = []
-        for itera, state in self.iter_states():
-            states.append((state, itera))
-        dlg = CustomRefreshDlg(states)
-        result = dlg.run()
-        [self.refresh_single(itera) for state, itera in result]
-        self.overall_update()
-        self.state_changed()
-    
-    def idle_func(self, func):
-        proc_id = gobject.idle_add(func.next)
-        self.active_processes.append(proc_id)
-        gobject.timeout_add(500, self.progress_update)       
-        
-    def progress_update(self):
-        self.progress.set_fraction(self.progress_percent)
-        if self.progress_percent == 1.0 or self.progress_percent == 1:
-            self.progress.set_text('Done')
-            return False
-        self.progress.set_text(self.progress_text)
-        return True
-    
-    def stop_refresh(self, *args):
-        remaining = filter(gobject.source_remove, self.active_processes)
-        self.active_processes = [x for x in self.active_processes if x not in remaining]
-        if not self.active_processes:
-            self.refreshing = False
+            self.overallList.append([cand['name'], cand['votes'], cand['delegates']])
     
     def iter_states(self):
         for row in self.stateList:
             yield row.iter, row[0]
-        
+    
     def clear_states(self):
         self.stateList.clear()
     
@@ -214,29 +217,31 @@ class CustomRefreshDlg(object):
     def __init__(self, states):
         self.gladefile = 'pyelection.glade'
         self.states = states
-    
-    def run(self):
+        
         self.wTree = gtk.glade.XML(self.gladefile, 'stateSelectorDlg')
         self.wTree.signal_autoconnect(self)
         self.dlg = self.wTree.get_widget('stateSelectorDlg')
         
         self.stateArea = self.wTree.get_widget('boxStateSelector')
         
-        for state, itera in self.states:
+        for itera, state in self.states:
             cbox = gtk.CheckButton(label=state.name)
             self.stateArea.pack_start(cbox)
         
         self.dlg.show_all()
-        
+    
+    def run(self):
         result = self.dlg.run()
         
         if result == gtk.RESPONSE_OK:
             active = [self.states[num] for num, chbox in enumerate(self.stateArea.get_children()) if chbox.get_active()]
-        
+        else:
+            active = []
         self.dlg.destroy()
         return active
 
-
 if __name__ == '__main__':
     app = pyelection()
+    gtk.gdk.threads_enter()
     gtk.main()
+    gtk.gdk.threads_leave()
